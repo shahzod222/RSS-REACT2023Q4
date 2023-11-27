7d4183add2e84aae3ee5e4a2006aca983137df7e
shahzod222 <shahzodubaydullaev2206@gmail.com>
2023-11-27 21:05:18 +0500

    feat: make sure nextJS works

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
   moduleNameMapper: {
     '^next/router$': '<rootDir>/node_modules/next/dist/client/router.js',
   },
